808d3e8190180b78a62425f3b3830a89edb17107
Harsh Tyagi <tyagih7654321@gmail.com>
2025-08-09 09:26:21 +0530

    Final Code

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,7 +7,7 @@ import App from "./App";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <App /> 
     </Router>
   </React.StrictMode>
 );
